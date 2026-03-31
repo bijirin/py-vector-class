8579b509963687a3a777b21f797374cd5949d2fa
bijirin <tsutrisn@gmail.com>
2026-03-31 21:32:07 +0700

    Refactor Vector class constructor and methods for improved clarity and consistency

--- app/main.py
+++ app/main.py
@@ -3,19 +3,19 @@ import math
 
 
 class Vector:
-    def __init__(self, x: float, y: float) -> None:
-        self.x = round(x, 2)
-        self.y = round(y, 2)
+    def __init__(self, vect_x: float, vect_y: float) -> None:
+        self.x = round(vect_x, 2)
+        self.y = round(vect_y, 2)
 
     def __add__(self, other: Vector) -> Vector:
         return Vector(self.x + other.x, self.y + other.y)
 
-    def __sub__(self, other) -> Vector:
+    def __sub__(self, other: Vector) -> Vector:
         return Vector(self.x - other.x, self.y - other.y)
 
     def __mul__(self, scalar: Vector | float | int) -> Vector:
         if isinstance(scalar, Vector):
-            return self.dot_product(scalar) 
+            return self.dot_product(scalar)
         return Vector(self.x * scalar, self.y * scalar)
 
     def __truediv__(self, scalar: float | int) -> Vector:
@@ -33,9 +33,12 @@ class Vector:
         start_point: tuple[float, float],
         end_point: tuple[float, float]
     ) -> Vector:
-        return Vector(end_point[0] - start_point[0], end_point[1] - start_point[1])
+        return Vector(
+            end_point[0] - start_point[0],
+            end_point[1] - start_point[1]   
+        )
 
-    def get_length(self) -> float:
+    def get_length(self) -> float:  
         return (self.x ** 2 + self.y ** 2) ** 0.5
 
     def get_normalized(self) -> Vector:
